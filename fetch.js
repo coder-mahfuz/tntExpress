2eefd76acd4dadbc677af57820fec297b4050edc
coder-mahfuz <mahfuz468865@gmail.com>
2022-10-28 18:47:05 +0600

    cleaned

--- fetch.js
+++ fetch.js
@@ -19,6 +19,10 @@ const payer = document.querySelector('#payer');
 const collectionDate = document.querySelector('#collection_date');
 const dangerousGoods = document.querySelector('#dgoods');
 
+
+const userName = "CIT00000000000144187";
+const password = "prodTNT123";
+const senderAccount = 20194618;
 // const userCurrentLocalDateTime = 
 
 const showCost = document.querySelector('#show_cost');
@@ -34,9 +38,9 @@ function showShippingCost(e) {
   myHeaders.append("Cookie", "ASPSESSIONIDQQTQATQT=AAGNPFADHOAEGFJJLGFHDMFJ; lb=rd1o00000000000000000000ffffcb1abeb0o443");
 
   var urlencoded = new URLSearchParams();
-  urlencoded.append("username", "CIT00000000000144187");
-  urlencoded.append("password", "prodTNT123");
-  urlencoded.append("xmlRequest", `<?xml version=\"1.0\"?> \n<enquiry xmlns=\"http://www.tntexpress.com.au\"> \n <ratedTransitTimeEnquiry> \n <cutOffTimeEnquiry> \n <collectionAddress> \n <suburb>${collectionSuburb.value}</suburb> \n <postCode>${collectionPostCode.value}</postCode> \n <state>${collectionState.value}</state> \n </collectionAddress> \n <deliveryAddress> \n <suburb>${deliverySuburb.value}</suburb> \n <postCode>${deliveryPostCode.value}</postCode> \n <state>${deliveryState}</state> \n </deliveryAddress> \n <shippingDate>2007-11-05</shippingDate> \n <userCurrentLocalDateTime> \n 2007-11-05T10:00:00 \n </userCurrentLocalDateTime> \n <dangerousGoods> \n <dangerous>${dangerousGoods.value}</dangerous> \n </dangerousGoods> \n <packageLines packageType=\"${goodsDescription.value}\"> \n <packageLine> \n <numberOfPackages>${quantity.value}</numberOfPackages> \n <dimensions unit=\"cm\"> \n <length>${length.valueOf}</length> \n <width>${width.valueOf}</width> \n <height>${height.valueOf}</height> \n </dimensions> \n <weight unit=\"kg\"> \n <weight>${weight.valueOf}</weight> \n </weight> \n </packageLine> \n </packageLines> \n </cutOffTimeEnquiry> \n <termsOfPayment> \n <senderAccount>20194618</senderAccount> \n <payer>${payer.value}</payer> \n </termsOfPayment> \n </ratedTransitTimeEnquiry> \n</enquiry>`);
+  urlencoded.append("username", userName);
+  urlencoded.append("password", password);
+  urlencoded.append("xmlRequest", `<?xml version=\"1.0\"?> \n<enquiry xmlns=\"http://www.tntexpress.com.au\"> \n <ratedTransitTimeEnquiry> \n <cutOffTimeEnquiry> \n <collectionAddress> \n <suburb>${collectionSuburb.value}</suburb> \n <postCode>${collectionPostCode.value}</postCode> \n <state>${collectionState.value}</state> \n </collectionAddress> \n <deliveryAddress> \n <suburb>${deliverySuburb.value}</suburb> \n <postCode>${deliveryPostCode.value}</postCode> \n <state>${deliveryState}</state> \n </deliveryAddress> \n <shippingDate>2007-11-05</shippingDate> \n <userCurrentLocalDateTime> \n 28/10/2022 07:44:16 \n </userCurrentLocalDateTime> \n <dangerousGoods> \n <dangerous>${dangerousGoods.value}</dangerous> \n </dangerousGoods> \n <packageLines packageType=\"${goodsDescription.value}\"> \n <packageLine> \n <numberOfPackages>${quantity.value}</numberOfPackages> \n <dimensions unit=\"cm\"> \n <length>${length.valueOf}</length> \n <width>${width.valueOf}</width> \n <height>${height.valueOf}</height> \n </dimensions> \n <weight unit=\"kg\"> \n <weight>${weight.valueOf}</weight> \n </weight> \n </packageLine> \n </packageLines> \n </cutOffTimeEnquiry> \n <termsOfPayment> \n <senderAccount>${senderAccount}</senderAccount> \n <payer>${payer.value}</payer> \n </termsOfPayment> \n </ratedTransitTimeEnquiry> \n</enquiry>`);
 
   var requestOptions = {
     method: 'POST',
